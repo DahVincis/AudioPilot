064df7a6627e515f0c4d82a8ab3ec87a1ef95e2a
Pedro H  L Fernandes <ph.leao2099@gmail.com>
2024-02-05 15:08:45 -0500

    modified:   client.py 	modified:   server.py

--- client.py
+++ client.py
@@ -5,9 +5,6 @@ X32_OSC_PORT = 10023
 
 client = udp_client.SimpleUDPClient(X32_IP_ADDRESS, X32_OSC_PORT)
 
-# Sending metering request for channel 18 (remember channels start at index 0, so channel 18 is index 17)
-client.send_message("/meters/6", [17])  # arguments should be in a list
-
-# Repeat for other channels
+client.send_message("/meters/6", [17])  # Channel 18
 client.send_message("/meters/6", [18])  # Channel 19
 client.send_message("/meters/6", [21])  # Channel 22
