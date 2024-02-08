c86fbcfb9417d10f2f99d21b9b15151204695b33
Pedro H  L Fernandes <ph.leao2099@gmail.com>
2024-02-08 13:45:29 -0500

    modified:   client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from pythonosc import udp_client
 
-X32_IP_ADDRESS = '192.168.56.1'  # Actual IP address of your X32 mixer
+X32_IP_ADDRESS = '192.168.56.1'  # IP address of X32 mixer
 X32_OSC_PORT = 10023
 
 client = udp_client.SimpleUDPClient(X32_IP_ADDRESS, X32_OSC_PORT)
