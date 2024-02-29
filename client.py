b8eb7921ae26371f9e210034de4eb94b8299ed82
DahVincis <ph.leao2099@gmail.com>
2024-02-29 00:49:37 -0500

    modified:   client.py 	new file:   test.py

--- client.py
+++ client.py
@@ -6,5 +6,6 @@ X32_OSC_PORT = 10023
 
 client = udp_client.SimpleUDPClient(X32_IP_ADDRESS, X32_OSC_PORT)
 
+client.send_message("/xremote", None)  # Send XRemote to keep connection alive
+client.send_message("/renew", ["/meters/15"]) # Renew RTA data
 client.send_message("/meters", ["/meters/6", [16]])  # Turn on DCA 7
-client.send_message("ch/01/mix/fader", None)  # Set channel 1 fader to 0.5
